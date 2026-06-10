6d0e489d454e308bab7ccce5854981f775cebe72
DaemonWoo <ivanovzakhary@gmail.com>
2026-06-10 21:48:55 +0300

    move up

--- basics/arrays/arr.sh
+++ basics/arrays/arr.sh
@@ -11,6 +11,21 @@ for color in "${colors[@]}"; do
 	echo "$color"
 done
 
+# get length
+echo "${#colors[@]}"
+
+# delete el
+unset 'colors[1]'
+
+echo "${#colors[@]}"
+
+# get all indices
+echo "${!colors[@]}"
+
+# slice
+echo "Slice:"
+echo "${colors[@]:0:3}"
+
 # map
 declare -A user
 user[name]="Alex"
@@ -28,3 +43,5 @@ echo "${map_colors[green]}"
 for key in "${!user[@]}"; do
   echo "$key -> ${user[$key]}"
 done
+
+echo "${!map_colors[@]}"
